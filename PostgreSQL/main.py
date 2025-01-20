443f05048e758a5665d16be75b212df5cb383f56
Lucas Queiroz <lucasqrz1@hotmail.com>
2025-01-20 16:52:16 -0300

    Update main.py

--- PostgreSQL/main.py
+++ PostgreSQL/main.py
@@ -8,3 +8,11 @@ conn = psycopg2.connect(
   user = "username" , 
   password = "password" 
 )
+
+#Data Exec
+cursor - conn.cursor()
+cursor.execute("SELECT * FROM my_table")
+records = cursor.fetchall()
+for record in records:
+  print(record)
+
