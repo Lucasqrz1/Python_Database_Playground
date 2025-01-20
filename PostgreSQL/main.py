f3f0c62dbf9291cfdb2f8e563efed522764b143c
Lucas Queiroz <lucasqrz1@hotmail.com>
2025-01-20 16:54:32 -0300

    Update main.py

--- PostgreSQL/main.py
+++ PostgreSQL/main.py
@@ -1,18 +1,22 @@
-#basic connect with database through postgresql
-
 import psycopg2
 
+# Database connection
 conn = psycopg2.connect(
-  host = "my_host",
-  database = "my_database" ,
-  user = "username" , 
-  password = "password" 
+    host="my_host",
+    database="my_database",
+    user="username",
+    password="password"
 )
 
-#Data Exec
-cursor - conn.cursor()
-cursor.execute("SELECT * FROM my_table")
-records = cursor.fetchall()
-for record in records:
-  print(record)
-
+try:
+    # Data Execution
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM my_table")
+    records = cursor.fetchall()
+    
+    for record in records:
+        print(record)
+finally:
+    # Close the cursor and connection
+    cursor.close()
+    conn.close()
